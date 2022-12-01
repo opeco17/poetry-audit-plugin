d513fad63c22b43ea023c817628e9c94e561cafa
franco0700 <franco20.fl@gmail.com>
2022-12-01 17:46:04 -0300

    Tests ready

--- tests/test_main.py
+++ tests/test_main.py
@@ -119,3 +119,95 @@ def test_vulnerabilities_in_main_dev_json_report(tmp_path: Path) -> None:
     assert dev_vulnerability in vulnerabilitie_names
     assert main_vulnerability in vulnerabilitie_names
     assert result.returncode == 1
+
+
+def test_supressed_vulnerabilities_in_main( tmp_path: Path) -> None:
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("vulnerabilities_in_main", testing_dir)
+    result = run_audit(testing_dir=testing_dir, args=["--ignore-code=CVE-2020-1735"])
+
+    assert "poetry audit report" in result.stdout
+    assert main_vulnerability in result.stdout
+    assert "vulnerabilities found in" in result.stdout
+    assert "vulnerabilities found but ignored" in result.stdout
+    assert "CVE-2020-1735" not in result.stdout
+    assert result.returncode == 1
+
+def test_supressed_vulnerabilities_in_dev( tmp_path: Path) -> None:
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("vulnerabilities_in_dev", testing_dir)
+    result = run_audit(testing_dir=testing_dir, args=["--ignore-code=CVE-2020-1735"])
+
+    assert "poetry audit report" in result.stdout
+    assert dev_vulnerability in result.stdout
+    assert "vulnerabilities found in" in result.stdout
+    assert "vulnerabilities found but ignored" in result.stdout
+    assert "CVE-2020-1735" not in result.stdout
+    assert result.returncode == 1
+
+def test_supressed_vulnerabilities_in_main_dev( tmp_path: Path) -> None:
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("vulnerabilities_in_main_dev", testing_dir)
+    result = run_audit(testing_dir=testing_dir, args=["--ignore-code=CVE-2020-1735"])
+
+    assert "poetry audit report" in result.stdout
+    assert dev_vulnerability in result.stdout
+    assert main_vulnerability in result.stdout
+    assert "vulnerabilities found in" in result.stdout
+    assert "vulnerabilities found but ignored" in result.stdout
+    assert "CVE-2020-1735" not in result.stdout
+    assert result.returncode == 1
+
+def test_supressed_packages_in_main( tmp_path: Path) -> None:
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("vulnerabilities_in_main", testing_dir)
+    result = run_audit(testing_dir=testing_dir, args=["--ignore-package=ansible-tower-cli"])
+
+    assert "poetry audit report" in result.stdout
+    assert "vulnerabilities found but ignored" in result.stdout
+    assert "ansible-tower-cli" not in result.stdout
+    assert result.returncode == 0
+
+def test_supressed_packages_in_main_dev( tmp_path: Path) -> None:
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("vulnerabilities_in_main_dev", testing_dir)
+    result = run_audit(testing_dir=testing_dir, args=["--ignore-package=ansible-tower-cli"])
+
+    assert "poetry audit report" in result.stdout
+    assert "vulnerabilities found but ignored" in result.stdout
+    assert "ansible-tower-cli" not in result.stdout
+    assert dev_vulnerability in result.stdout
+    assert result.returncode == 1
+
+
+def test_supressed_vulnerabilities_in_main_dev_json_report(tmp_path: Path) -> None:
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("vulnerabilities_in_main_dev", testing_dir)
+    result = run_audit(testing_dir=testing_dir, args=["--json", "--ignore-code=CVE-2020-1735"])
+    result_dict = json.loads(result.stdout)
+    vulnerabilitie_names = []
+    vulnerabilitie_cve = []
+    for vuln in result_dict["vulnerabilities"]:
+        vulnerabilitie_names.append(vuln["name"])
+        for detail in vuln["vulns"]:
+            vulnerabilitie_cve.append(detail["cve"])
+
+    assert dev_vulnerability in vulnerabilitie_names
+    assert main_vulnerability in vulnerabilitie_names
+    assert "CVE-2020-1735" not in vulnerabilitie_cve
+    assert result.returncode == 1
+
+
+def test_ignored_packages_in_main_dev_json_report(tmp_path: Path) -> None:
+    testing_dir = tmp_path / "testing_package"
+    copy_assets("vulnerabilities_in_main_dev", testing_dir)
+    result = run_audit(testing_dir=testing_dir, args=["--json", "--ignore-package=ansible-tower-cli"])
+    result_dict = json.loads(result.stdout)
+    vulnerabilitie_names = []
+    vulnerabilitie_cve = []
+    for vuln in result_dict["vulnerabilities"]:
+        vulnerabilitie_names.append(vuln["name"])
+
+    assert dev_vulnerability in vulnerabilitie_names
+    assert "ansible-tower-cli" not in vulnerabilitie_names
+    assert result.returncode == 1
