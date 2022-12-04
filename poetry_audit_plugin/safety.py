b63ae7fd7133b4debd720013ed56708c305c45c8
opeco17 <opeco17@gmail.com>
2022-12-04 12:40:36 +0900

    Refactoring the code

--- poetry_audit_plugin/safety.py
+++ poetry_audit_plugin/safety.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Iterator, List
+from typing import Any, Dict, Iterator, List, Tuple
 
 from packaging.specifiers import SpecifierSet
 from safety.safety import fetch_database
@@ -10,7 +10,7 @@ class Package:
         self.version = version
 
 
-class VulnerabilityDetail:
+class Vulnerability:
     def __init__(self, cve: str, spec: str, advisory: str) -> None:
         self.cve = cve
         self.spec = spec
@@ -20,17 +20,17 @@ class VulnerabilityDetail:
         return {"cve": self.cve, "affectedVersion": self.spec, "advisory": self.advisory}
 
 
-class Vulnerability:
-    def __init__(self, name: str, version: str, details: List[VulnerabilityDetail]) -> None:
+class VulnerablePackage:
+    def __init__(self, name: str, version: str, vulnerabilities: List[Vulnerability]) -> None:
         self.name = name
         self.version = version
-        self.details = details
+        self.vulnerabilities = vulnerabilities
 
     def format(self) -> Dict[str, Any]:
         return {
             "name": self.name,
             "installedVersion": self.version,
-            "vulns": [detail.format() for detail in self.details],
+            "vulns": [vulnerability.format() for vulnerability in self.vulnerabilities],
         }
 
 
@@ -41,13 +41,13 @@ def get_vulnerable_entry(pkg_name: str, spec: str, db_full: Dict[str, Any]) -> I
                 yield entry
 
 
-def check_vulnerabilities(packages: List[Package]) -> List[Vulnerability]:
+def check_vulnerable_packages(packages: List[Package]) -> List[VulnerablePackage]:
     db: Dict[str, Any] = fetch_database()
     db_full: Dict[str, Any] = {}
-    vulnerable_packages: List[Vulnerability] = []
+    vulnerable_packages: List[VulnerablePackage] = []
     for pkg in packages:
         name = pkg.name.replace("_", "-").lower()
-        details: List[VulnerabilityDetail] = []
+        vulnerabilities: List[Vulnerability] = []
         if name in frozenset(db.keys()):
             specifiers: List[str] = db[name]
             for specifier in specifiers:
@@ -60,11 +60,46 @@ def check_vulnerabilities(packages: List[Package]) -> List[Vulnerability]:
                         if cve:
                             cve = cve.split(",")[0].strip()
                         if data.get("id"):
-                            details.append(
-                                VulnerabilityDetail(advisory=data.get("advisory", ""), cve=cve, spec=specifier)
+                            vulnerabilities.append(
+                                Vulnerability(advisory=data.get("advisory", ""), cve=cve, spec=specifier)
                             )
 
-        if details:
-            vulnerable_packages.append(Vulnerability(name=name, version=pkg.version, details=details))
+        if vulnerabilities:
+            vulnerable_packages.append(
+                VulnerablePackage(name=name, version=pkg.version, vulnerabilities=vulnerabilities)
+            )
 
     return vulnerable_packages
+
+
+def suppress_vulnerable_packages(
+    vulnerable_packages: List[VulnerablePackage], ignored_packages: List[str], ignored_codes: List[str]
+) -> Tuple[List[VulnerablePackage], int]:
+    filtered_vulnerable_packages: List[VulnerablePackage] = []
+    amount_of_ignored_vulnerabilities = 0
+
+    is_ignore_packages = len(ignored_packages) > 0
+    is_ignore_codes = len(ignored_codes) > 0
+
+    for vulnerable_package in vulnerable_packages:
+        if is_ignore_packages:
+            if vulnerable_package.name in ignored_packages:
+                amount_of_ignored_vulnerabilities += len(vulnerable_package.vulnerabilities)
+                continue
+
+        if is_ignore_codes:
+            filtered_vulnerabilities: List[Vulnerability] = []
+            for vulnerability in vulnerable_package.vulnerabilities:
+                if vulnerability.cve not in ignored_codes:
+                    filtered_vulnerabilities.append(vulnerability)
+                else:
+                    amount_of_ignored_vulnerabilities += 1
+
+            if len(filtered_vulnerabilities):
+                vulnerable_package.vulnerabilities = filtered_vulnerabilities
+            else:
+                continue
+
+        filtered_vulnerable_packages.append(vulnerable_package)
+
+    return filtered_vulnerable_packages, amount_of_ignored_vulnerabilities
